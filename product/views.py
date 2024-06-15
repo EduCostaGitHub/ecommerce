5a60a83b26aa7ae4269de4bf7791667a562ed8af
Eduardo Costa <educosta.costa@gmail.com>
2024-06-15 23:38:57 +0100

    class views list and detail

--- product/views.py
+++ product/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.contrib import messages
 from django.views import View
 from django.http import HttpResponse
-from product.models import Product
+from product.models import Product, ProductType
 
 # Create your views here.
 
@@ -22,8 +24,44 @@ class ProductDetail(DetailView):
 
 
 class AddToCart(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('Add To Cart')
+    def get(self, *args, **kwargs): 
+        # get product variation id       
+        p_id = self.request.GET.get('p_id')
+
+        # if product variation not exist set error message and return to home
+        if not p_id:
+            messages.error(
+                self.request,
+                'Product not found!',
+            )
+            return redirect(reverse('product:list'))
+        # get product ( product variation) to add to cart
+        p_to_add = get_object_or_404(ProductType, id = p_id)
+
+        #check if cart exist, if not save one
+        if not self.request.session.get('cart'):
+            self.request.session['cart'] = {}
+            self.request.session.save()
+        #get cart, new or existing
+        cart = self.request.session['cart']
+
+        if p_id in cart:
+            # TODO: product exist in cart
+            pass
+        else:
+            # TODO: product does not exist in cart
+            pass
+        
+        return HttpResponse(f'{p_to_add.product} {p_to_add.name}')
+
+        # messages.error(
+        #     self.request,
+        #     'Testing error messages!'
+        # )
+
+        # return redirect(self.request.META['HTTP_REFERER'])
+
+        
 
 class RemoveFromCart(View):
     def get(self, *args, **kwargs):
